0207ae90e42d3d0aff4f7d86b2be7c4c3ce16fda
hadistar <hadistar0417@gmail.com>
2021-04-13 14:57:03 +0900

    first

--- test.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-data_filter = pd.read_csv('D:/OneDrive - SNU/data/filterpm25conc_6sites.csv', encoding='euc-kr')
-
-data_filter = data_filter.loc[data_filter['ID']=='Seoul']
-
-data_filter = data_filter.to_datetime
