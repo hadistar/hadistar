0565f371ee0d3111eecb9cf8f350de03ba0e078f
hadistar <hadistar0417@gmail.com>
2021-04-15 23:11:50 +0900

    test

---
+++ tf_GPU_test.py
@@ -0,0 +1,113 @@
+import csv
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+#from google.colab import files
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        # Currently, memory growth needs to be the same across GPUs
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    except RuntimeError as e:
+        # Memory growth must be set before GPUs have been initialized
+        print(e)
+
+
+def get_data(filename):
+    with open(filename) as training_file:
+        csv_reader = csv.reader(training_file, delimiter=',')
+        first_line = True
+        temp_images = []
+        temp_labels = []
+        for row in csv_reader:
+            if first_line:
+                # print("Ignoring first line")
+                first_line = False
+            else:
+                temp_labels.append(row[0])
+                image_data = row[1:785]
+                image_data_as_array = np.array_split(image_data, 28)
+                temp_images.append(image_data_as_array)
+        images = np.array(temp_images).astype('float')
+        labels = np.array(temp_labels).astype('float')
+    return images, labels
+
+
+training_images, training_labels = get_data('sign_mnist_train.csv')
+testing_images, testing_labels = get_data('sign_mnist_test.csv')
+
+print(training_images.shape)
+print(training_labels.shape)
+print(testing_images.shape)
+print(testing_labels.shape)
+
+
+training_images = np.expand_dims(training_images, axis=3)
+testing_images = np.expand_dims(testing_images, axis=3)
+
+train_datagen = ImageDataGenerator(
+    rescale=1. / 255,
+    rotation_range=40,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest')
+
+validation_datagen = ImageDataGenerator(
+    rescale=1. / 255)
+
+print(training_images.shape)
+print(testing_images.shape)
+
+
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(128, activation=tf.nn.relu),
+    tf.keras.layers.Dense(26, activation=tf.nn.softmax)])
+
+model.compile(optimizer = tf.optimizers.Adam(),
+              loss = 'sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+history = model.fit(train_datagen.flow(training_images, training_labels, batch_size=32),
+                              steps_per_epoch=len(training_images) / 32,
+                              epochs=15,
+                              validation_data=validation_datagen.flow(testing_images, testing_labels, batch_size=32),
+                              validation_steps=len(testing_images) / 32)
+
+model.evaluate(testing_images, testing_labels)
+
+
+
+
+import matplotlib.pyplot as plt
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(len(acc))
+
+plt.plot(epochs, acc, 'r', label='Training accuracy')
+plt.plot(epochs, val_acc, 'b', label='Validation accuracy')
+plt.title('Training and validation accuracy')
+plt.legend()
+plt.figure()
+
+plt.plot(epochs, loss, 'r', label='Training Loss')
+plt.plot(epochs, val_loss, 'b', label='Validation Loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()
