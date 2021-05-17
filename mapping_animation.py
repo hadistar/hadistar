c0ff11cf3defee9b242a178cd9ab7733f5ce5b13
hadistar <hadistar0417@gmail.com>
2021-05-17 13:01:51 +0900

    BSMRM

--- mapping_animation.py
+++ mapping_animation.py
@@ -3,20 +3,26 @@ import pandas as pd
 import branca.colormap as cm
 from selenium import webdriver
 import time
+import math
 
 df = pd.read_csv('Anew_source1.csv')
 
-m = folium.Map(location=[35.5502, 126.982], zoom_start=7,
-                tiles='cartodbpositron')
 
-colormap = cm.LinearColormap(colors = ['white','yellow','orange','red'],
-                             index = [0,15,30,45],
-#                             tick_labels=[0,15,30,45],
-#                             scale_width=400, scale_heigh=50,
-                             vmin=0, vmax=45,
-                             caption='concentration (ug/m3)')
 
 for i, day in enumerate(df.columns[2:]):
+    m = folium.Map(location=[35.5502, 126.982],
+                   zoom_start=7,
+                   tiles='cartodbpositron',
+                   control_scale=True)
+
+    c_max = math.ceil(df.iloc[:,i+2].max())
+    colormap = cm.LinearColormap(colors=['white', 'yellow', 'orange', 'red'],
+                                 index=[0, c_max/3, c_max*2/3, c_max],
+                                 #                             tick_labels=[0,15,30,45],
+                                 #                             scale_width=400, scale_heigh=50,
+                                 vmin=0, vmax=c_max,
+                                 caption='concentration (ug/m3)')
+
     print(i, day)
     for pt in range(int(len(df)/4)):
         pt = pt *4
