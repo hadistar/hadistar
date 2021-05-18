7b4efc0a815e64c114346d2b38df0c6a252ce2e6
hadistar <hadistar0417@gmail.com>
2021-05-18 12:42:38 +0900

    BSMRM

--- mapping_animation.py
+++ mapping_animation.py
@@ -6,10 +6,14 @@ import time
 import math
 import matplotlib.pyplot as plt
 import cartopy
+import numpy as np
 plt.rcParams['font.family'] = 'Arial'
 plt.rcParams['font.size'] = 20
 
-df = pd.read_csv('Anew_source1.csv')
+df = pd.read_csv('Anew_source1.csv', header=None)
+s2 = pd.read_csv('s2.csv')
+s_new = pd.read_csv('s_new.csv')
+date = pd.read_csv('date.csv')
 
 # 1. Using Folium
 
@@ -50,36 +54,43 @@ for i, day in enumerate(df.columns[2:]):
     browser.save_screenshot('./test/test_'+str(day).replace('/','_')+'.png')
     browser.quit()
 
-# Using Pandas and Cartopy
+# 2. Using Pandas and Cartopy
 
+for i in range(len(df.columns)):
+    day = date.iloc[i].date
+    print(i, day)
 
-fig = plt.figure(figsize=(10,10))
+    df_z = df.iloc[:,i]
 
-ax = plt.axes(projection=cartopy.crs.PlateCarree())
+    plt.figure(figsize=(10,10))
+    ax = plt.axes(projection=cartopy.crs.PlateCarree())
 
-ax.add_feature(cartopy.feature.LAND)
-ax.add_feature(cartopy.feature.OCEAN)
-ax.add_feature(cartopy.feature.COASTLINE)
-ax.add_feature(cartopy.feature.BORDERS, linestyle=':')
-ax.add_feature(cartopy.feature.LAKES, alpha=0.5)
-ax.add_feature(cartopy.feature.RIVERS)
+    ax.add_feature(cartopy.feature.LAND)
+    ax.add_feature(cartopy.feature.OCEAN, facecolor='lightblue')
+    ax.add_feature(cartopy.feature.COASTLINE)
+    ax.add_feature(cartopy.feature.BORDERS, linestyle=':')
+    ax.add_feature(cartopy.feature.LAKES, alpha=0.5)
+    ax.add_feature(cartopy.feature.RIVERS)
 
-lon1, lon2, lat1, lat2 = 124, 130, 33, 39
-ax.set_extent([lon1, lon2, lat1, lat2], crs=cartopy.crs.PlateCarree())
-gl = ax.gridlines(draw_labels=True, crs=cartopy.crs.PlateCarree(), linestyle='--')
-gl.xlabel_style = {'size': 15}
-gl.ylabel_style = {'size': 15}
+    lon1, lon2, lat1, lat2 = 123.5, 130.5, 32.5, 39.5
+    ax.set_extent([lon1, lon2, lat1, lat2], crs=cartopy.crs.PlateCarree())
+    gl = ax.gridlines(draw_labels=True, crs=cartopy.crs.PlateCarree(), linestyle='--')
+    gl.xlabel_style = {'size': 15}
+    gl.ylabel_style = {'size': 15}
 
-import numpy as np
-lon = np.linspace(125,128,100)
-lat = np.linspace(35,38,100)
-data = np.linspace(10,20,100)
+    #plt.plot(np.nan, np.nan, color=None, markersize=0, label='['+df.iloc[:,2].name+']')
+    plt.plot(s2['lon'], s2['lat'], color='blue', marker='X',
+             linestyle='None' , markersize=15, label='Monitoring Site')
 
-points = plt.scatter(lon, lat, c=data, cmap='Reds', alpha=0.5)
-cb = plt.colorbar(points, orientation='vertical',ticklocation='auto', shrink=0.6, pad=0.1)
-cb.set_label(label='Concentration (ug/m3)', size=20)
-cb.ax.tick_params(labelsize=20)
-fig.tight_layout()
+    points = plt.scatter(s_new['lon'], s_new['lat'], c=df_z,
+                         vmin=0, vmax=math.ceil(df_z.max()) + 5 - math.ceil(df_z.max())%5,
+                         cmap='Reds', alpha=0.8, s=3.0)
+    cb = plt.colorbar(points, orientation='vertical',ticklocation='auto', shrink=0.5, pad=0.1)
 
+    cb.set_label(label='Concentration ('+ "${\mu}$" +'g/m' + r'$^3$' + ')', size=20)
+    cb.ax.tick_params(labelsize=20)
+    plt.tight_layout()
 
-plt.show()
+    plt.legend(title='['+day+']', loc='upper right')
+    plt.savefig('./test/test_'+str(day).replace('/','_')+'.jpg')
+    plt.close()
