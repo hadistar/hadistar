4b08756adc03a9f0289eef3d76ec568c4d5a32a2
hadistar <hadistar0417@gmail.com>
2021-05-29 15:39:00 +0900

    For comparison

---
+++ Matlab/Comparison_BNFA_PMF_210529.m
@@ -0,0 +1,81 @@
+G_q5 = readtable('.\PMF_forcomparison_210508\210525q5_contributions2.csv');
+P_q5 = readtable('.\PMF_forcomparison_210508\210525q5_profiles3.csv');
+species_name = table2cell(P_q5(:,1));
+G_q5 = table2array(G_q5(:, 2:end));
+P_q5 = table2array(P_q5(:, 2:end));
+
+G_q6 = readtable('.\PMF_forcomparison_210508\210525q6_contributions2.csv');
+P_q6 = readtable('.\PMF_forcomparison_210508\210525q6_profiles3.csv');
+G_q6 = table2array(G_q6(:, 2:end));
+P_q6 = table2array(P_q6(:, 2:end));
+
+G_q7 = readtable('.\PMF_forcomparison_210508\210529q7_contributions2.csv');
+P_q7 = readtable('.\PMF_forcomparison_210508\210529q7_profiles3.csv');
+G_q7 = table2array(G_q7(:, 2:end));
+P_q7 = table2array(P_q7(:, 2:end));
+
+G_q8 = readtable('.\PMF_forcomparison_210508\210529q8_contributions2.csv');
+P_q8 = readtable('.\PMF_forcomparison_210508\210529q8_profiles3.csv');
+G_q8 = table2array(G_q8(:, 2:end));
+P_q8 = table2array(P_q8(:, 2:end));
+
+G_q9 = readtable('.\PMF_forcomparison_210508\210529q9_contributions2.csv');
+P_q9 = readtable('.\PMF_forcomparison_210508\210529q9_profiles3.csv');
+G_q9 = table2array(G_q9(:, 2:end));
+P_q9 = table2array(P_q9(:, 2:end));
+
+G_q10 = readtable('.\PMF_forcomparison_210508\210529q10_contributions2.csv');
+P_q10 = readtable('.\PMF_forcomparison_210508\210529q10_profiles3.csv');
+G_q10 = table2array(G_q10(:, 2:end));
+P_q10 = table2array(P_q10(:, 2:end));
+
+
+scale = sum(P_q5);
+scaled_profile = (P_q5./scale)*100;
+sum(scaled_profile)
+scaled_contribution = G_q5.*(scale/100);
+
+
+[q J] = size(scaled_profile')
+temp = scaled_profile';
+
+figure
+for k=1:q
+    subplot(q,1,k)
+    P_bargraph = temp(k,:);
+    
+    bar(1:J,P_bargraph )
+    set(gca,'xtick',[1:J],'xticklabel',species_name)
+    end
+    axis auto
+    ylabel(['Source ',num2str(k)])
+    if k==1
+        title('Source Composition Profiles')
+    elseif k==q
+        xlabel('Species')
+    end
+    
+    
+temp = scaled_contribution';
+
+figure
+for k=1:q
+    subplot(q,1,k)
+    plot(dtks,temp(k,:),'-')
+    axis tight
+    legend(['Source ',num2str(k)],'Location','North')   
+    
+    set(gca,'XMinorTick','on','YMinorTick','on')
+    set(gca,'XTick', dtks)
+    datetick('x', 'mmm-dd-yy')
+    
+    if k==1
+        legend('Source 1','95% PI','Location','North')
+        title('Source Contributions')
+    elseif k==2
+        ylabel('Mass concentration')
+    elseif k==q
+        xlabel('Observation #')
+    end
+end
+
