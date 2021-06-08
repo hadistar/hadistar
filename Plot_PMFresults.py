ab2c0dcab54f07cfda5bec76aff16e75e3c53870
hadistar <hadistar0417@gmail.com>
2021-06-08 16:42:23 +0900

    Plot_source profile and contribution

--- Plot_PMFresults.py
+++ Plot_PMFresults.py
@@ -2,25 +2,120 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.read_csv('210607recal_ConstrainedErrorEstimationSummary_v1.csv')
+df_contri = pd.read_csv('210607recal_ConstrainedErrorEstimationSummary_v1_contribution.csv').dropna()
+df_contri['date'] = pd.to_datetime(df_contri['date'], format='%m-%d-%y %H:%M')
 
 plt.rcParams['font.family'] = 'Arial'
 plt.rcParams['font.size'] = 13
 
-# Value들 PM2.5로 나눠야함..
-# y축 색 빨간색으로,
-
-fig, ax1 = plt.subplots()
-
-ax1.bar(df.Species, df.loc[:,'Constrained Base Value_Salts'])
-ax1.bar(df.Species, df.loc[:,'DISP Average_Salts'], yerr=df.loc[:,'DISP Average_Salts'], capsize=10)
-ax1.plot(df.Species, df.loc[:,'DISP Average_Salts'], 'yerr=df.loc[:,'DISP Average_Salts'], capsize=10')
-ax1.set_yscale('log')
+Sources_name = ['Sea salts','Soil', 'Combustion for heating',
+                'Coal combustion','Secondary sulfate','Industry (smelting)',
+                'Secondary Nitrate','Mobile','Biomass burning','Industry (oil)']
 
 
-ax2 = ax1.twinx() # Create another axes that shares the same x-axis as ax.
-ax2.plot(df.Species, df.loc[:,'EV_Salts'], 'ro')#, width=0.3)
-ax2.set_ylim([0,100])
+data = pd.DataFrame()
+data['Species'] = df['Species']
+
+for i, s in enumerate(Sources_name):
+    data['Conc_'+s] = df.iloc[:,4*i+1] / df.iloc[:,4*i+1][0]
+    data['Min_'+s] = df.iloc[:,4*i+2] / df.iloc[:,4*i+1][0]
+    data['Avg_'+s] = df.iloc[:,4*i+3] / df.iloc[:,4*i+1][0]
+    data['Max_'+s] = df.iloc[:,4*i+4] / df.iloc[:,4*i+1][0]
+    data['EV_'+s] = df.iloc[:,41+i]
+
+data = data.drop(0, axis=0)
+
+
+Species_name = list(data.Species)
+Species_name[:6] = ['NO$_3$$^-$', 'SO$_4$$^{2-}$', 'NH$_4$$^+$', 'K$^+$', 'Na$^+$', 'Cl$^-$']
+
+# Source profile plot
+
+fig, axes = plt.subplots(nrows=10, ncols=1, figsize=(12,24), sharey=True, sharex=True)
+
+for i, s in enumerate(Sources_name):
+
+    error = [(data.loc[:,'Avg_'+s] - data.loc[:,'Min_'+s]),
+             (data.loc[:,'Max_'+s] - data.loc[:,'Avg_'+s])] # [(Bottom range), (Upper range)]
+
+    ax1 = axes[i]
+    ax2 = ax1.twinx()
+
+    ax1.bar(Species_name, data.loc[:,'Conc_'+s], color='silver',
+            yerr=error, capsize=5)
+    #ax1.set_ylabel('Concentration ('+ "${\mu}$" +'g/'+"${\mu}$" +'g)')
+    ax1.set_yscale('log')
+    ax1.set_ylim([1e-4,2])
+    ax1.tick_params(axis='y', colors='black')
+    ax1.set_yticks([1e-4,1e-3,1e-2,1e-1,1])
+
+    ax2.plot(Species_name, data.loc[:,'EV_'+s], 'ro')#, width=0.3)
+    ax2.set_ylim([0,100])
+    ax2.set_yticks([0,25,50,75,100])
+    ax2.yaxis.label.set_color('red')
+
+    ax2.spines['right'].set_color('red')
+    ax2.tick_params(axis='y', colors='red')
+
+    ax1.text(19,0.4, s, size=20, ha='center')
+
+#fig.supylabel('Concentration ('+ "${\mu}$" +'g/'+"${\mu}$" +'g)')
+fig.text(0.05, 0.5, 'Concentration ('+ "${\mu}$" +'g/'+"${\mu}$" +'g)', ha='center',va='center',rotation='vertical', fontsize=20)
+fig.text(0.95, 0.5, 'Percent of species (%)', ha='center',va='center',rotation=270, fontsize=20, color='red')
 
 fig.autofmt_xdate(rotation=45)
+plt.show()
 
+
+# Contribution plot
+
+fig, axes = plt.subplots(nrows=10, ncols=1, figsize=(12,24), sharex=True)
+
+for i, s in enumerate(Sources_name):
+    ax1 = axes[i]
+    ax1.plot(df_contri['date'], df_contri[s], 'k-')
+    ax1.fill_between(df_contri['date'],0,df_contri[s], color='lightgrey')
+    ax1.text(1.0,0.85,
+             s+" "+str(df_contri[s].mean().round(2))+" ${\mu}$" +'g/m'+"$^3$",
+             size=14, ha='right', transform=ax1.transAxes)
+    ax1.set_ylim(bottom=0)
+
+fig.text(0.05, 0.5, 'Mass Concentration ('+ "${\mu}$" +'g/m'+"$^3$"+")",
+         ha='center',va='center',rotation='vertical', fontsize=16)
+fig.autofmt_xdate(rotation=45)
+plt.show()
+
+
+
+
+# For Backup
+
+fig, axes = plt.subplots(nrows=10, ncols=1, figsize=(12,24), sharey=True, sharex=True)
+ax1 = axes[0]
+#fig, ax1 = plt.subplots(10,1, figsize=(12,4))
+ax2 = ax1.twinx()
+
+ax1.bar(Species_name, data.loc[:,'Conc_Sea salts'], color='silver',
+        yerr=error, capsize=5)
+#ax1.set_ylabel('Concentration ('+ "${\mu}$" +'g/'+"${\mu}$" +'g)')
+ax1.set_yscale('log')
+ax1.set_ylim([1e-4,2])
+ax1.tick_params(axis='y', colors='black')
+ax1.set_yticks([1e-4,1e-3,1e-2,1e-1,1])
+
+ax2.plot(Species_name, data.loc[:,'EV_Sea salts'], 'ro')#, width=0.3)
+ax2.set_ylim([0,100])
+ax2.set_yticks([0,25,50,75,100])
+ax2.yaxis.label.set_color('red')
+
+ax2.spines['right'].set_color('red')
+ax2.tick_params(axis='y', colors='red')
+
+ax1.text(19,0.4, 'Sea salts', size=20, ha='center')
+
+#fig.supylabel('Concentration ('+ "${\mu}$" +'g/'+"${\mu}$" +'g)')
+fig.text(0.05, 0.5, 'Concentration ('+ "${\mu}$" +'g/'+"${\mu}$" +'g)', ha='center',va='center',rotation='vertical', fontsize=20)
+fig.text(0.95, 0.5, 'Explained value', ha='center',va='center',rotation=270, fontsize=20, color='red')
+
+fig.autofmt_xdate(rotation=45)
 plt.show()
